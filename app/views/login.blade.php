995e3651827766b3b1e8408de5af3cc9d63e1cd9
Ezequiel Gonzalez Garcia <ezeezegg@gmail.com>
2015-06-10 17:22:33 -0500

    fixt it mobile version for view login.blade.php

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <title>Sistema de Control de Gestión</title>  
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" type="text/css" href="{{ asset('css/normalize.css') }}"/>
   <link rel="stylesheet" href="{{ asset('bootstrap-3.2.0/css/bootstrap.min.css') }}">
   <link rel="stylesheet" href="{{ asset('css/utils.css') }}">
@@ -10,45 +11,47 @@
   {{ HTML::style('assets/css/signin.css') }}
 </head>
 <body>
-  <div class="col-md-3"></div>
-  <div class="container center col-md-6" style="background-image:url({{asset('img/incan.jpg')}})">
-    <div class="fluid center">
-      {{ Form::open(['url' => 'login', 'autocomplete' => 'off', 'class' => 'form-signin', 'role' => 'form']) }}
+  
+  <div class="container center " style="background-image:url({{asset('img/incan.jpg')}})">
+    <div class="row">
+      <div class="fluid center">
+        {{ Form::open(['url' => 'login', 'autocomplete' => 'off', 'class' => 'form-signin', 'role' => 'form']) }}
 
-      <h2 class="form-signin-heading center">Sistema de Control de Gestión</h2>
+        <h2 class="form-signin-heading center">Sistema de Control de Gestión</h2>
 
-      {{ Form::label('username', 'Username', ['class' => 'sr-only']) }}
-      {{ Form::text('username', null, ['class' => 'form-control', 'placeholder' => 'Username', 'autofocus' => '']) }}
-      </br>
-      {{ Form::label('password', 'Password', ['class' => 'sr-only']) }}
-      {{ Form::password('password', ['class' => 'form-control', 'placeholder' => 'Contraseña']) }}
-      </br>
-      </br>
-      <div class="checkbox">
-        <label>
-          {{ Form::checkbox('remember', true) }} Recuerdame
-        </label>
+        {{ Form::label('username', 'Username', ['class' => 'sr-only']) }}
+        {{ Form::text('username', null, ['class' => 'form-control', 'placeholder' => 'Username', 'autofocus' => '']) }}
+        </br>
+        {{ Form::label('password', 'Password', ['class' => 'sr-only']) }}
+        {{ Form::password('password', ['class' => 'form-control', 'placeholder' => 'Contraseña']) }}
+        </br>
+        </br>
+        <div class="checkbox">
+          <label>
+            {{ Form::checkbox('remember', true) }} Recuerdame
+          </label>
+        </div>
+        <p class="center">
+        {{ Form::submit('Iniciar Sesión', ['class' => 'btn btn-primary ']) }}
+        </p>
+
+        {{ Form::close() }}
+        <!--<a class="btn btn-success" href="{{ action('AuthController@registerUser') }}">Create User</a>-->
+        <a class="btn btn-success center" href="{{ action('UserController@showPassRecovery') }}">Olvide mi contraseña</a>
+        @if(Session::has('error_message'))
+          <div class="alert alert-danger">
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+            {{ Session::get('error_message') }}
+        @endif
+        </div>
+        </br>
+        </br>
+        </br>
+        </br>
+        </br>
       </div>
-      <p class="center">
-      {{ Form::submit('Iniciar Sesión', ['class' => 'btn btn-primary ']) }}
-      </p>
-
-      {{ Form::close() }}
-      <!--<a class="btn btn-success" href="{{ action('AuthController@registerUser') }}">Create User</a>-->
-      <a class="btn btn-success center" href="{{ action('UserController@showPassRecovery') }}">Olvide mi contraseña</a>
-      @if(Session::has('error_message'))
-        <div class="alert alert-danger">
-          <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-          {{ Session::get('error_message') }}
-      @endif
-      </div>
-      </br>
-      </br>
-      </br>
-      </br>
-      </br>
     </div>
-    <div class="col-md-3"></div>
+    
   </div>
   <script src="//code.jquery.com/jquery.js"></script>
   <script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
